5b43c86df57592e881c818b72eb322fe92b8109a
BenGorrr <ggbengaben@gmail.com>
2021-06-15 23:08:54 +0800

    Added markers for both players

--- game.py
+++ game.py
@@ -52,6 +52,19 @@ def main():
     clock = pygame.time.Clock()
     # board_surf, board_squares = create_board()
     board = Board(600, 600) #create a 600x600 board
+    turn_counters = 1
+    player1_markers = []
+    player2_markers = []
+    winning_pairs = [
+        [0, 1, 2], #horizontal1
+        [3, 4, 5], #horizontal2
+        [6, 7, 8], #horizontal3
+        [0, 3, 6], #vertical1
+        [1, 4, 7], #vertical2
+        [2, 5, 8], #vertical3
+        [0, 4, 8], #diagonal1
+        [2, 4, 6], #diagonal2
+    ]
 
     while not done: # game loop
         for event in pygame.event.get():
@@ -59,23 +72,41 @@ def main():
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONUP:
                 #check if mouse click on any squares
-                # square = isInSquare(event.pos, board.board_squares)
+                # python 3.8 Walrus Operator
+                # if (square := board.selectedSquares(event.pos)):
                 square = board.selectedSquares(event.pos)
                 if square: #if clicked on square
-                    pygame.draw.rect(board.board_surf, LIGHTGREEN, square)
-                    #print(square)
-                # python 3.8 Walrus Operator
-                # if (square := isInSquare(event.pos, board_squares)):
-                #     pygame.draw.rect(board_surf, LIGHTGREEN, square)
+                    if square not in player1_markers and square not in player2_markers:
+                        if turn_counters % 2 != 0: # odd counter = player1's turn
+                            player1_markers.append(square)
+                        elif turn_counters % 2 == 0: # even counter = player2's turn
+                            player2_markers.append(square)
+
+                        # get index of square marker to do winning check
+                        p1_markers_index = list(map(board.board_squares.index, player1_markers))
+                        p2_markers_index = list(map(board.board_squares.index, player2_markers))
+
+                        for win_pair in winning_pairs: #iterate all the winning pairs
+                            #use all() to check if every marker in that win_pair is in the player markers list
+                            if all(marker in p1_markers_index for marker in win_pair):
+                                print("Player 1 WON!")
+                                pygame.draw.rect(board.board_surf, LIGHTGREEN, square)
+                            elif all(marker in p2_markers_index for marker in win_pair):
+                                print("Player 2 WON!")
+                                pygame.draw.rect(board.board_surf, LIGHTGREEN, square)
+
+                        turn_counters += 1 # increment counter for next turn
+
 
         # Fill the background with black
         screen.fill((0, 0, 0))
-        # pygame.display.update()
         # blit the board on the screen
         screen.blit(board.board_surf, board.board_surf.get_rect())
         # Draw squares
         # board_squares = drawSquares(board_surf, board_squares)
         board_squares = board.drawSquares()
+        board.drawMarker(player1_markers, "o")
+        board.drawMarker(player2_markers, "x")
         pygame.display.flip()
         clock.tick(10)
 
@@ -116,6 +147,17 @@ class Board():
             if square.collidepoint(pos):
                 # return board_squares.index(square)
                 return square #return square rect obj
+
+    def drawMarker(self, squaresList, marker_type):
+        if marker_type == "o":
+            for square in squaresList:
+                pygame.draw.circle(self.board_surf, WHITE, square.center, self.SQUARE_OFFSET/2 - 15, 4)
+        elif marker_type == "x":
+            padding = 30
+            for square in squaresList:
+                pygame.draw.line(self.board_surf, WHITE, (square.left + padding, square.top + padding), (square.right - padding, square.bottom - padding), 4)
+                pygame.draw.line(self.board_surf, WHITE, (square.left + padding, square.bottom - padding), (square.right - padding, square.top + padding), 4)
+
 
 
 if __name__ == '__main__':
